0f3cc5d4ba6d9edea1720e58474099548ac924f5
V4p1d <p.ferraro@imperial.ac.uk>
2021-10-28 12:29:37 +0100

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,7 +1,7 @@
 # Write a program that receives two inputs from the terminal (using input()).
 # Both inputs need to be an integer. Assign them, respectively to a variable named "n"
 # and a variable named "p". The program will 
-# compute the product n*nn*nnn*...*nnn...n (where the last number contains n written p number of times) 
+# compute the product n*nn*nnn*...*nnn...n (where the last number contains n written p number of times)
 # and print it on the screen.
 # example 1: n = 5, p = 4, output = 5*55*555*5555 = 152625
 # example 2: n = 3, p = 6, output = 3*33*333*3333*33333*333333 = 1220864470355308779
