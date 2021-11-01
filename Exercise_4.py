0823fb6eae3dc7dcc7255377f5293aee6ae91f5f
alice-mackenzie <amm521@ic.ac.uk>
2021-11-01 15:57:29 +0000

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -3,3 +3,13 @@
 # However, if two of the three inputs are equal, the program will print the product of these three inputs.
 # Example 1: n = 1, m = 2, l = 3, output = 1 + 2 + 3 = 6
 # Example 2: n = 1, m = 2, l = 2, output = 1 * 2 * 2 = 4
+
+a, b, c = int(input()), int(input ()), int(input())
+
+if a==b or b==c or a==c:
+    if a==b==c:
+        print(a+b+c)
+    else:
+        print(a*b*c)
+else:
+    print(a+b+c)
