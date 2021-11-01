1082cf5ac57138488919de2e2dc18d01f30688d7
alice-mackenzie <amm521@ic.ac.uk>
2021-11-01 15:57:20 +0000

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,3 +2,20 @@
 # This input needs to be an integer. Assign it to a variable named "n". The program will 
 # compute the product n*nn*nnn and print it on the screen.
 # example: n = 5 output = 5*55*555 = 152625
+
+#n = int(input())
+
+#output = (n*n*(11)*n*(111))
+
+#print(output)
+
+n = input()
+nn = int(n*2)
+nnn = int(n*3)
+n = int(n)
+
+print(n*nn*nnn)
+
+#print(n)
+#print(nn)
+#print(nnn)
