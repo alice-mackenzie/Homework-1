f69e07b7b8254d8c4c72355a0ee4dd1145cd4722
alice-mackenzie <amm521@ic.ac.uk>
2021-11-01 15:57:27 +0000

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,3 +2,13 @@
 # This input needs to be a string. If the string is "This is the right string." the program ends.
 # Otherwise it keeps printing "Please provide the right string" until the string "This is the right string." 
 # is provided.
+
+isCorrectString = False
+correctString = "This is the right string."
+
+while isCorrectString == False:
+    inputString = str(input())
+    if inputString == correctString:
+        isCorrectString = True
+    else:
+        print ("Please provide the right string:")
